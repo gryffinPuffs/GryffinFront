82a5b2e643cc8c1911d5c8b60742661047e18c94
Jenniffer Melchiade <jdenisemelch@gmail.com>
2022-11-29 15:14:31 -0500

    imported components on main to render

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,17 +1,25 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import {Navbar} from "./"
+import {
+  Navbar,
+  Home,
+  Login,
+  Cart,
+  Register,
+  Profile,
+  SeeCartDetails,
+  SeeProductDetails,
+  SingleProduct,
+} from "./";
 
 const Main = () => {
   return (
     <div id="main">
-<Navbar/>
-<Routes>
-
-<Route path="/" element={<Home/>}></Route>
-
-</Routes>
-  </div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </div>
   );
 };
 
