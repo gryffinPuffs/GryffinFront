3aa3bf789ca999fb03cfadd88ae8b70289f68c2f
Jenniffer Melchiade <jdenisemelch@gmail.com>
2022-11-29 19:21:59 -0500

    added navbar img

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,46 +1,48 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
-
+import accioScroll from "./AccioScroll.png";
 
 const Navbar = (props) => {
-  const setLoggedIn = props.setLoggedIn
-  const loggedIn = props.loggedIn
+  const setLoggedIn = props.setLoggedIn;
+  const loggedIn = props.loggedIn;
 
   return (
     <>
-    <div id="navbar">
-      <h2> Accio Books!</h2>
-      <div className="links">
+      <div id="navbar">
+        <div>
+          <img id="accioImg" src={accioScroll} alt="Accio Logo"></img>
+        </div>
+        <h2> Accio Books!</h2>
+        <div className="links">
           <NavLink className="linkBar" to="/">
             Home
           </NavLink>
-          {
-            loggedIn ?
-            (
-              <>
-              <NavLink className="linkBar" onClick={()=>{
-                localStorage.removeItem("token");
-                setLoggedIn(false);
-              }}>LogOut</NavLink>
+          {loggedIn ? (
+            <>
+              <NavLink
+                className="linkBar"
+                onClick={() => {
+                  localStorage.removeItem("token");
+                  setLoggedIn(false);
+                }}
+              >
+                LogOut
+              </NavLink>
               <NavLink className="linkBar" to="/profile">
                 Profile
               </NavLink>
-              </>
-            ): (<NavLink className="linkBar" to="/login">
+            </>
+          ) : (
+            <NavLink className="linkBar" to="/login">
               Login
             </NavLink>
-            )
-
-          }
-          <NavLink className="linkBar"to="/cart">Cart</NavLink>
+          )}
+          <NavLink className="linkBar" to="/cart">
+            Cart
+          </NavLink>
         </div>
-
-
-    </div>
+      </div>
     </>
-
-
-
   );
 };
 
