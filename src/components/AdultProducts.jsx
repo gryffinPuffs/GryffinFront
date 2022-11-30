79ca898c018dd4ac42e58fc496a6e0cc6a591c55
Jenniffer Melchiade <jdenisemelch@gmail.com>
2022-11-30 12:00:23 -0500

    added map for adult books

--- src/components/AdultProducts.jsx
+++ src/components/AdultProducts.jsx
@@ -1,22 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const AdultProducts = () => {
-  // const BASE_URL = "http://localhost:8080/api";
-  // const [allAdultProducts, setAllAdultProducts] = userState({})
+  const BASE_URL = "http://localhost:8080/api";
+  const [allAdultProducts, setAllAdultProducts] = useState({});
 
-  // useEffect(() => {
-  //   const getProductByAudience = async () => {
-  //     const response = await fetch(`BASE_URL${audience}`
-  //     );
-  //   const result = await response.json();
-  //   setAllAdultProducts(result);
-  // };
-  // getProductByAudience()}, []);
+  useEffect(() => {
+    const getProductByAudience = async () => {
+      const response = await fetch(`${BASE_URL}/product/adult`);
+      const result = await response.json();
+      setAllAdultProducts(result);
+    };
+    getProductByAudience();
+  }, []);
 
   return (
     <div id="adultproducts">
-      {/* {allAdultProducts && allAdultProducts.length ? allAdultProducts} */}
-      <h2> No Adult Books</h2>
+      {allAdultProducts && allAdultProducts.length ? (
+        allAdultProducts.map((product) => {
+          return (
+            <div id="adultBooks" key={`product-${product.id}`}>
+              <div className="bookImg">{product.image_url}</div>
+              <div className="title">Book Name: {product.name}</div>
+              <div className="price">Book Price: {product.price}</div>
+              <div className="description">
+                Book description: {product.description}
+              </div>
+            </div>
+          );
+        })
+      ) : (
+        <h2>No Adult Books Available</h2>
+      )}
     </div>
   );
 };
