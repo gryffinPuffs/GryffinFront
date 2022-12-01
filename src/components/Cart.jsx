16df5ee5d84aad40ad88b6be4582dbf8a1e43078
Jessy <japarker0421@gmail.com>
2022-12-01 15:09:35 -0700

    updated price, and quantity

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -13,6 +13,10 @@ useEffect(()=>{
   }
   getUserCart();
 }, []);
+function price(product, quantity){
+  const thePrice= (product * quantity)/100
+  return thePrice
+}
 
 console.log(theCart, "should be a cart")
   return (
@@ -22,7 +26,8 @@ console.log(theCart, "should be a cart")
       theCart.map((product)=>{
         return(<div className="cartProds" key={`products-${product.id}`}>
           <div>{product.name}</div>
-          <div>{product.price}</div>
+          <div>Price: ${price(product.price, product.quantity)}</div>
+          <div>{product.quantity}</div>
           </div>
         )
       }): <h2>Nothing in cart yet. Find some books!</h2> }
