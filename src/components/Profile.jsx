a008889fa44a80a339cc10d7584b3273604dd01c
Randy Pellegrin <rpell391@gmail.com>
2022-12-04 12:45:04 -0600

    create product button working

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,46 +1,42 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { LoggedIn } from "./"
-import { useNavigate } from "react-router-dom"
+import { LoggedIn } from "./";
+import { useNavigate } from "react-router-dom";
 
 const Profile = (props) => {
-  const navigate= useNavigate()
+  const navigate = useNavigate();
   const user = props.user;
-console.log(user)
-  return(
-   <div>
-   <h2>profile</h2>
-   <LoggedIn user = {user} />
+  console.log(user);
+  return (
+    <div>
+      <h2>profile</h2>
+      <LoggedIn user={user} />
 
+      <button type="purchase history" className="purchaseHistory">
+        Purchase History
+      </button>
 
+      <button type="User Info" className="userInfo">
+        User Info
+      </button>
 
-   <button type="purchase history" className="purchaseHistory">Purchase History</button>
-   
-   <button type="User Info" className="userInfo">User Info</button>
-
-    {user && user.admin ? (<>
-
-    <Link to={"/users"}><button type="all users" className="allUsers">All Users</button></Link>
-    <button type="Create Product" className="createProduct">Create Product</button>
-
-    <button type="all users" className="allUsers">All Users</button>
-    <button type="Create Product" className="createProduct" onClick={()=>{navigate("/makeproduct")}} >Create Product</button>
-
-    </>) : (null)}
-
+      {user && user.admin ? (
+        <>
+          <Link to={"/users"}>
+            <button type="all users" className="allUsers">
+              All Users
+            </button>
+          </Link>
+          <Link to={"/makeproduct"}><button type="Create Product" className="createProduct">
+            Create Product
+          </button></Link>
+        </>
+      ) : null}
     </div>
-  )
-}
-
-
-
-
-
-
+  );
+};
 
 //buttons for Address, purchase History, wish list,
-
-
 
 //Add user purchase history (have not created)
 //Add user wish list (need to create component)
