8bc11126aa8b5c53250b319c6b95b3f999b255f8
Jenniffer Melchiade <jdenisemelch@gmail.com>
2022-12-05 15:56:38 -0500

    revised adminbooks with edit and delete functions

--- src/AdminBooks.js
+++ src/AdminBooks.js
@@ -1,10 +1,23 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { createProduct, getAllProducts } from "./components/api-adapter";
+import {
+  createProduct,
+  getAllProducts,
+  deleteProduct,
+  updateProduct,
+} from "./components/api-adapter";
 import Book from "./components/Book";
 import { NavLink } from "react-router-dom";
 const AdminBooks = ({ bookInfo, setBookInfo, allBooks, setAllBooks }) => {
   const navigate = useNavigate();
+  const [update, setUpdate] = useState(false);
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [image, setImage] = useState("");
+  const [image2, setImage2] = useState("");
+  const [audience, setAudience] = useState("");
+  const [author, setAuthor] = useState("");
+  const [description, setDescription] = useState("");
 
   useEffect(() => {
     async function fetchAllBooks() {
@@ -31,6 +44,55 @@ const AdminBooks = ({ bookInfo, setBookInfo, allBooks, setAllBooks }) => {
     }
   }
 
+  //admin access only - delete single product
+  async function handleDeleteAdmin(e) {
+    e.preventDefault();
+    const destroyedProduct = await deleteProduct(bookInfo);
+    console.log("ITS DESTROY PRODUCT", destroyedProduct);
+    const availableProducts = allBooks.filter((product) => {
+      console.log(product, "THIS IS PRODUCT");
+      return product.id !== destroyedProduct.id;
+    });
+    setAllBooks([availableProducts, ...allBooks]);
+    navigate("/allbooks");
+  }
+
+  async function handleUpdateAdmin(e) {
+    e.preventDefault();
+    const updatedProduct = await updateProduct(
+      bookId,
+      name,
+      price,
+      image,
+      image2,
+      author,
+      audience,
+      description
+    );
+    console.log(bookId, "This is book info");
+    console.log(updatedProduct, "UPDATED");
+
+    const editedProducts = allBooks.filter((product) => {
+      console.log(product, "THIS IS PRODUCT");
+      return product.id == updatedProduct.id;
+    });
+    setAllBooks([editedProducts, ...allBooks]);
+    navigate("/singleproduct");
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    console.log("banana");
+    setName(singleBook.name);
+    setAuthor(singleBook.author);
+    setAudience(singleBook.audience);
+    setDescription(singleBook.description);
+    setImage(singleBook.image_url);
+    setImage2(singleBook.image_url2);
+    setPrice(singleBook.price);
+    setUpdate(true);
+  }
+
   return (
     <div id="allBooks">
       <h2> You may Edit these books</h2>
@@ -47,6 +109,82 @@ const AdminBooks = ({ bookInfo, setBookInfo, allBooks, setAllBooks }) => {
               );
             })
           : null}
+      </div>
+      <button onClick={handleDeleteAdmin}>Delete Book</button>
+      <button onClick={handleSubmit}>Edit Book </button>
+      <div>
+        {update ? (
+          <>
+            <form>
+              <input
+                placeholder="Book Title"
+                className="Book Title"
+                type="text"
+                value={name}
+                onChange={(event) => {
+                  setName(event.target.value);
+                }}
+              ></input>
+              <input
+                placeholder="Price"
+                className="Price"
+                type="text"
+                value={price}
+                onChange={(event) => {
+                  setPrice(event.target.value);
+                }}
+              ></input>
+              <input
+                placeholder="Image URL"
+                className="Image URL"
+                type="text"
+                value={image}
+                onChange={(event) => {
+                  setImage(event.target.value);
+                }}
+              ></input>
+              <input
+                placeholder="Image URL"
+                className="Image URL"
+                type="text"
+                value={image2}
+                onChange={(event) => {
+                  setImage2(event.target.value);
+                }}
+              ></input>
+              <input
+                placeholder="Audience Type"
+                className="audienceType"
+                type="text"
+                value={audience}
+                onChange={(event) => {
+                  setAudience(event.target.value);
+                }}
+              ></input>
+              <input
+                placeholder="Author"
+                className="author"
+                type="text"
+                value={author}
+                onChange={(event) => {
+                  setAuthor(event.target.value);
+                }}
+              ></input>
+              <input
+                placeholder="Description"
+                className="description"
+                type="text"
+                value={description}
+                onChange={(event) => {
+                  setDescription(event.target.value);
+                }}
+              ></input>
+              <button onClick={handleUpdateAdmin} type="submit">
+                Submit
+              </button>
+            </form>
+          </>
+        ) : null}
       </div>
     </div>
   );
