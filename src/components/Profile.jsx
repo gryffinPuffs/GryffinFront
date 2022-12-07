040282259451a049c1a0a2aa11bb29a344cf4d27
Randy Pellegrin <rpell391@gmail.com>
2022-12-07 12:24:46 -0600

    corrected error from last merge

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -9,18 +9,12 @@ const Profile = (props) => {
   console.log(user);
   return (
     <div>
-
       <LoggedIn user={user} />
-      </div>
 
-     <div id="profileLinks"> 
+      
      <Link to={"/userinfo"}><button type="User Info" className="buttons">
         User Info
       </button></Link>
-      <Link to={"/favorite"}><button type="favorite" className="buttons">
-        Favorites
-      </button></Link>
-      
 
       {user && user.admin ? (
         <>
@@ -33,7 +27,7 @@ const Profile = (props) => {
             Create Product
           </button></Link>
         </>
-      ) : null}</div>
+      ) : null}
     </div>
   );
 };
